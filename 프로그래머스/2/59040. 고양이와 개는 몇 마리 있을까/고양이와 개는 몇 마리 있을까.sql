5b8e63c3c2e6a53888a700b1f996ccbb6da1ec37
HYUNG BIN LEE <114133335+hbin0529@users.noreply.github.com>
2026-03-10 11:40:09 +0900

    [level 2] Title: 고양이와 개는 몇 마리 있을까, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59040. 고양이와 개는 몇 마리 있을까/고양이와 개는 몇 마리 있을까.sql
@@ -0,0 +1,13 @@
+-- ANIMAL_INS
+-- ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE
+-- 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부
+/*
+문제: 동물 보호소에 들어온 동물 중 고양이와 개가 각각 몇 마리인지 조회하는 SQL문을 작성해주세요. 
+이때 고양이를 개보다 먼저 조회해주세요.
+*/
+SELECT ANIMAL_TYPE, COUNT(ANIMAL_ID)
+FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+ORDER BY 1
+
+
