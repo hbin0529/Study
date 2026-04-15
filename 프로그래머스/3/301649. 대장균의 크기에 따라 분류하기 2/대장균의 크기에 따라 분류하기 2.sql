1a73eeeb20b6ca0acfc3a9093be77b20b1bccbdf
HYUNG BIN LEE <114133335+hbin0529@users.noreply.github.com>
2026-04-15 10:44:49 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -0,0 +1,12 @@
+-- 코드를 작성해주세요
+SELECT ID
+     , CASE ASD
+       WHEN 1 THEN 'CRITICAL'
+       WHEN 2 THEN 'HIGH'
+       WHEN 3 THEN 'MEDIUM'
+       WHEN 4 THEN 'LOW'
+       END AS COLONY_NAME
+  FROM (SELECT ID
+             , NTILE(4) OVER (ORDER BY SIZE_OF_COLONY DESC) AS ASD
+          FROM ECOLI_DATA) T
+ ORDER BY ID ASC;
